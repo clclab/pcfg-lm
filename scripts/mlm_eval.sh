0446ec9ff79ee3add3ab4fdd66a6a1472f691a0f
jumelet <jumeletjaap@gmail.com>
2025-01-22 12:42:09 +0100

    add mlm pipeline

---
+++ scripts/mlm_eval.sh
@@ -0,0 +1,4 @@
+java -cp src/data_generation/berkeley_parser/CustomBerkeley.jar edu.berkeley.nlp.PCFGLA.BerkeleyParser -gr $1 -inputFile $2
+
+#python src/lm_eval/eval_clm.py --model $5 --corpus $3 --pcfg_scores $4
+
---
+++ scripts/pipeline_mlm.sh
@@ -0,0 +1,34 @@
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+done
+
+bash scripts/induce_grammar.sh ${TREEBANK_SIZE} ${MERGE_PERCENTAGE} ${SM_CYCLES}
+
+python src/data_generation/generate.py \
+    -g resources/grammars/nltk/${TREEBANK_SIZE}_pcfg.txt \
+    -o ${DATA_OUTPUT:-resources/corpora/${TREEBANK_SIZE}} \
+    --min_length ${MIN_LENGTH:-3} \
+    --max_length ${MAX_LENGTH:-30} \
+    --corpus_size ${CORPUS_SIZE:-10000} \
+    --split_ratio ${SPLIT_RATIO:-0.8/0.1/0.1}
+    
+bash scripts/train_mlm.sh \
+    --data.data_dir resources/corpora/$TREEBANK_SIZE \
+    --trainer.output_dir resources/models/$TREEBANK_SIZE \
+    --model.hidden_size ${HIDDEN_SIZE:-16} \
+    --model.num_hidden_layers ${NUM_HIDDEN_LAYERS:-2} \
+    --model.intermediate_size ${INTERMEDIATE_SIZE:-16} \
+    --model.num_attention_heads ${NUM_ATTENTION_HEADS:-2} \
+    --trainer.num_train_epochs ${NUM_TRAIN_EPOCHS:-1} \
+    --trainer.hub_model_id ${HUB_MODEL_ID:-none} \
+    --trainer.hub_token hf_token.txt
+
+bash scripts/mlm_eval.sh \
+    resources/grammars/berkeley/${TREEBANK_SIZE}/${TREEBANK_SIZE} \
+    resources/corpora/${TREEBANK_SIZE}/test.txt
