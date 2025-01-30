85d3457169f120fc6f3238b1c25eaf68e862776d
Martijn van Beers <mail_dev@martijn.at>
2025-01-30 17:30:20 +0100

    Use datasetdict in training

--- src/lm_training/data.py
+++ src/lm_training/data.py
@@ -1,7 +1,7 @@
 import os
 from typing import Optional, Tuple
 
-from datasets import DatasetDict, load_dataset
+from datasets import DatasetDict
 from transformers import PreTrainedTokenizerFast
 
 
@@ -19,50 +19,20 @@ def tokenize_wrapper(tokenizer):
 
 def load_data(
     tokenizer: PreTrainedTokenizerFast,
-    data_dir: str,
+    dsd: DatasetDict,
     train_size: Optional[int] = None,
     dev_size: Optional[int] = None,
     test_size: Optional[int] = None,
-    train_file: str = "train.txt",
-    dev_file: str = "dev.txt",
-    test_file: str = "test.txt",
-    eval_file: Optional[str] = None,
 ) -> DatasetDict:
-    raw_train = load_dataset("text", data_files=os.path.join(data_dir, train_file))[
-        "train"
-    ]
-    raw_dev = load_dataset("text", data_files=os.path.join(data_dir, dev_file))["train"]
-    raw_test = load_dataset("text", data_files=os.path.join(data_dir, test_file))[
-        "train"
-    ]
-    if eval_file is not None:
-        raw_eval = load_dataset("text", data_files=os.path.join(data_dir, eval_file))[
-            "train"
-        ]
-    else:
-        raw_eval = None
 
-    if train_size is not None:
-        raw_train = raw_train.shuffle().select(range(train_size))
-    if dev_size is not None:
-        raw_dev = raw_dev.shuffle().select(range(dev_size))
-    if test_size is not None:
-        raw_test = raw_test.shuffle().select(range(test_size))
+    for split, size in zip(['train', 'dev', 'test'], [train_size, dev_size, test_size]):
+        if size is not None:
+            dsd[split] = dsd[split].filter(lambda e,i: i < size, with_indices=True)
 
-    dataset_dict = {
-        "train": raw_train,
-        "valid": raw_dev,
-        "test": raw_test,
-    }
-
-    if raw_eval is not None:
-        dataset_dict["eval"] = raw_eval
-
-    raw_datasets = DatasetDict(dataset_dict)
-
-    tokenized_datasets = raw_datasets.map(
-        tokenize_wrapper(tokenizer),
-        batched=True,
-    )
-
-    return tokenized_datasets
+    return (dsd
+            .shuffle()
+            .map(
+                tokenize_wrapper(tokenizer),
+                batched=True,
+            )
+        )
